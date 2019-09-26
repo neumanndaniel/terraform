bed8b43bd521e0af69a6506634f57b6ed4fa8056
Daniel Neumann <13654904+neumanndaniel@users.noreply.github.com>
2019-09-26 09:43:03 +0200

    Update output.tf

--- azure/aks-aad-custom-vnet/output.tf
+++ azure/aks-aad-custom-vnet/output.tf
@@ -1,6 +1,7 @@
 output "fqdn" {
-  value = "${azurerm_kubernetes_cluster.k8s.fqdn}"
+  value = azurerm_kubernetes_cluster.k8s.fqdn
 }
+
 output "id" {
-  value = "${azurerm_kubernetes_cluster.k8s.id}"
+  value = azurerm_kubernetes_cluster.k8s.id
 }
