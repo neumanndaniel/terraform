b3097682ff99b03ec0802772b830a7dadce2131e
Daniel Neumann <13654904+neumanndaniel@users.noreply.github.com>
2025-09-24 09:17:02 +0200

    Update diagnostic_logs module

--- modules/diagnostic_logs/main.tf
+++ modules/diagnostic_logs/main.tf
@@ -1,38 +1,28 @@
-####################
-# Provider section #
-####################
-provider "azurerm" {
-  version = ">= 2.0.0"
-  features {}
-}
-#####################
-# Resources section #
-#####################
-resource "azurerm_monitor_diagnostic_setting" "diagnostic_logs" {
-  name               = var.name
+resource "azurerm_monitor_diagnostic_setting" "diagnostic_logs_storage" {
+  for_each           = var.backend_type == "storage_account" ? toset(["default"]) : []
+  name               = "${var.name}-storage"
   target_resource_id = var.target_resource_id
   storage_account_id = var.storage_account_id
 
-  dynamic "log" {
+  dynamic "enabled_log" {
     for_each = var.diagnostic_logs
     content {
-      category = log.value
-      enabled  = true
-
-      retention_policy {
-        enabled = true
-        days    = var.retention
-      }
+      category = enabled_log.value
     }
   }
+}
 
-  metric {
-    category = "AllMetrics"
-    enabled  = false
+resource "azurerm_monitor_diagnostic_setting" "diagnostic_logs_log_analytics" {
+  for_each                       = var.backend_type == "log_analytics" ? toset(["default"]) : []
+  name                           = "${var.name}-log-analytics"
+  target_resource_id             = var.target_resource_id
+  log_analytics_workspace_id     = var.log_analytics_workspace_id
+  log_analytics_destination_type = "Dedicated"
 
-    retention_policy {
-      days    = 0
-      enabled = false
+  dynamic "enabled_log" {
+    for_each = var.diagnostic_logs
+    content {
+      category = enabled_log.value
     }
   }
 }
--- modules/diagnostic_logs/variables.tf
+++ modules/diagnostic_logs/variables.tf
@@ -3,6 +3,15 @@ variable "name" {
   type        = string
 }
 
+variable "backend_type" {
+  description = "The backend type where the diagnostic logs will be sent. Possible values are 'storage_account', or 'log_analytics'"
+  type        = string
+  validation {
+    condition     = var.backend_type == "storage_account" || var.backend_type == "log_analytics"
+    error_message = "The backend type must be either 'storage_account' or 'log_analytics'"
+  }
+}
+
 variable "target_resource_id" {
   description = "The resource id of the target resource for which diagnostic logs will be enabled"
   type        = string
@@ -11,14 +20,16 @@ variable "target_resource_id" {
 variable "storage_account_id" {
   description = "The resource id of the Storage Account where the diagnostic logs will be stored"
   type        = string
+  default     = null
+}
+
+variable "log_analytics_workspace_id" {
+  description = "The resource id of the Log Analytics workspace where the diagnostic logs will be stored"
+  type        = string
+  default     = null
 }
 
 variable "diagnostic_logs" {
   description = "The list of diagnostic logs that should be enabled"
   type        = list(string)
-}
-
-variable "retention" {
-  description = "The retention time for all diagnostic logs"
-  type        = number
 }
